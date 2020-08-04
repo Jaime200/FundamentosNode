659c0a0028b8038cd75ca57c385e6d7b448d8e3f
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-08-03 23:29:22 -0600

    2 callback anidados con parametros

--- async/callback.js
+++ async/callback.js
@@ -1,14 +1,26 @@
-function soyAsincrona (miCallback){
+function hola (nombre,miCallback){
     setTimeout(() => {
-        console.log('Hola Soy una funcion asincrona');    
-        miCallback();
+        console.log('Hola ' + nombre);    
+        miCallback(nombre);
     }, 1000);
 }
+
+function adios(nombre, otroCallback){
+    setTimeout(() => {
+        console.log('Adios '+ nombre);
+        otroCallback();
+    }, 1000);
+}
+
+
 console.log('Iniciando proceso...');
-soyAsincrona(
-    function () {
-        console.log('Terminando proceso');
-    }
-);
+hola('Jaime', function(nombre){
+    adios(nombre,function(){
+        console.log('Terminando Proceso....');
+    });
+    
+});
 
 
+// hola('Jaime', function(){});
+// adios('Jaime', function(){});s

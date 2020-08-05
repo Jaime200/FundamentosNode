eb6a2a21f3a944e5f36ccce23103343a714d339c
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-08-05 09:27:18 -0600

    Se agrego archivo donde se maneja los process

---
+++ modulos/process.js
@@ -0,0 +1,22 @@
+//const process = require('process');
+process.on('beforeExit', ()=>{
+    console.log('El proceso terminará');
+})
+
+
+process.on('exit', ()=>{
+    console.log('Saliendo del programa');
+    setTimeout(() => {
+        console.log('Esto no se ejecutara');
+    }, 0);
+})
+
+
+process.on('uncaughtException',(err,origen)=>{
+    console.error('Capturando el error');
+    console.error(err);
+    console.error(origen);
+});
+// process.on('uncaughtRejection');
+
+// funcionNoExiste();
